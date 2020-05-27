d271fe72a7fff6df20b28cab14825b6d15d35977
Clark Kent with glasses <61812163+locutusOO1@users.noreply.github.com>
2020-05-26 20:37:09 -0500

    updated input list

--- script.js
+++ script.js
@@ -38,4 +38,26 @@ function renderTodos() {
     }
 }
 
+// # Add ToDo's
+
+// In this activity, we will be continuing to build on our Todo activity. This time, we'll be adding the `add` functionality.
+
+// ## Instructions
+
+// * Add an event listener so that when a user hits enter, the value from the todo input field is pushed to our todo array.
+
+// * Make sure that empty values are not pushed to the array.
+
+// * Once the value has been added to the array, clear the input field and re-render the todo list.
+
+todoForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    var newTodoText = todoInput.value;
+    if (newTodoText !== "") {
+        todos.push(newTodoText);
+        todoInput.value = "";
+        renderTodos();
+    }
+})
+
 renderTodos();
